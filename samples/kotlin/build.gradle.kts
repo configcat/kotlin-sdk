f942c33067ca7e0a34fced2036ee2e439daa3be6
Peter Csajtai <peter.csajtai@outlook.com>
2022-08-04 11:53:52 +0200

    Add kotlin sample [skip ci]

---
+++ samples/kotlin/build.gradle.kts
@@ -0,0 +1,28 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.7.10"
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven {
+        url = uri("https://oss.sonatype.org/content/repositories/snapshots")
+    }
+}
+
+dependencies {
+    implementation("com.configcat:configcat-kotlin-client:0.1.0-SNAPSHOT")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClass.set("MainKt")
+}
---
+++ samples/kotlin/src/main/kotlin/Main.kt
@@ -0,0 +1,20 @@
+import com.configcat.ConfigCatClient
+import com.configcat.ConfigCatUser
+import com.configcat.getValue
+import com.configcat.log.LogLevel
+
+suspend fun main(args: Array<String>) {
+    val client = ConfigCatClient("PKDVCLf-Hq-h-kCzMp-L7Q/HhOWfwVtZ0mb30i9wi17GQ") {
+        // Info level logging helps to inspect the feature flag evaluation process.
+        // Use the default Warning level to avoid too detailed logging in your application.
+        logLevel = LogLevel.INFO
+    }
+
+    // Get individual config values identified by a key for a user.
+    println("isAwesomeFeatureEnabled: ${client.getValue("isAwesomeFeatureEnabled", defaultValue = false)}")
+
+    // Create a user object to identify the caller.
+    val user = ConfigCatUser(identifier = "configcat@example.com", email = "configcat@example.com")
+    val isPOCFeatureEnabled = client.getValue(key = "isPOCFeatureEnabled", defaultValue = false, user = user)
+    println("isPOCFeatureEnabled: $isPOCFeatureEnabled")
+}
