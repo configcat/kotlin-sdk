aeb6a79ab0eeb390b5623b5c225f8cb8879a6b7b
Peter Csajtai <peter.csajtai@outlook.com>
2022-08-09 20:22:58 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -157,10 +157,6 @@ kotlin {
             }
         }
 
-        val androidTest by getting {
-            dependsOn(commonTest)
-        }
-
         val darwinMain by creating {
             dependsOn(commonMain)
             dependencies {
