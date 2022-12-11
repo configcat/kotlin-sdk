ffb1120dc6184704956a05b5fa3b35c93a6127af
Peter Csajtai <peter.csajtai@outlook.com>
2022-12-11 15:37:55 +0100

    Darwin cache

--- src/darwinMain/kotlin/com/configcat/Cache.kt
+++ src/darwinMain/kotlin/com/configcat/Cache.kt
@@ -1,5 +1,17 @@
 package com.configcat
 
+import platform.Foundation.NSUserDefaults
+
 internal actual fun defaultCache(): ConfigCache {
-    return EmptyConfigCache()
+    return UserDefaultsCache()
+}
+
+internal class UserDefaultsCache : ConfigCache {
+    private val userDefaults = NSUserDefaults.standardUserDefaults
+
+    override suspend fun read(key: String): String? = userDefaults.stringForKey("com.configcat-$key")
+
+    override suspend fun write(key: String, value: String) {
+        userDefaults.setObject(value, "com.configcat-$key")
+    }
 }
