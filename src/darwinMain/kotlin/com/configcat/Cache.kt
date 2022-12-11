850a36dd689561990801715946e25d609f642c8d
Peter Csajtai <peter.csajtai@outlook.com>
2022-12-11 19:43:25 +0100

    Update Cache.kt

--- src/darwinMain/kotlin/com/configcat/Cache.kt
+++ src/darwinMain/kotlin/com/configcat/Cache.kt
@@ -8,10 +8,11 @@ internal actual fun defaultCache(): ConfigCache {
 
 internal class UserDefaultsCache : ConfigCache {
     private val userDefaults = NSUserDefaults.standardUserDefaults
+    private val prefix = "com.configcat"
 
-    override suspend fun read(key: String): String? = userDefaults.stringForKey("com.configcat-$key")
+    override suspend fun read(key: String): String? = userDefaults.stringForKey("$prefix-$key")
 
     override suspend fun write(key: String, value: String) {
-        userDefaults.setObject(value, "com.configcat-$key")
+        userDefaults.setObject(value, "$prefix-$key")
     }
 }
