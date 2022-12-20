ac43585052d6091bd200020f87df6512a23f3472
Peter Csajtai <peter.csajtai@outlook.com>
2022-12-20 17:20:47 +0100

    Bump version in samples

--- samples/js/build.gradle.kts
+++ samples/js/build.gradle.kts
@@ -7,13 +7,14 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    mavenLocal()
     maven {
         url = uri("https://oss.sonatype.org/content/repositories/snapshots")
     }
 }
 
 dependencies {
-    implementation("com.configcat:configcat-kotlin-client:1.0.0")
+    implementation("com.configcat:configcat-kotlin-client:1.1.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
     implementation("org.jetbrains.kotlin-wrappers:kotlin-react:18.0.0-pre.332-kotlin-1.6.21")
     implementation("org.jetbrains.kotlin-wrappers:kotlin-react-dom:18.0.0-pre.332-kotlin-1.6.21")
