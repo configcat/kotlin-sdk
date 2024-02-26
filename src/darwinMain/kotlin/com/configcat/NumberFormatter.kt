5fdd4e924b4d61701cb5dabd9756c16780c91580
novalisdenahi <novalisdenahi@gmail.com>
2024-02-26 13:59:13 +0100

    NSNumberFormatter added (not tested)

--- src/darwinMain/kotlin/com/configcat/NumberFormatter.kt
+++ src/darwinMain/kotlin/com/configcat/NumberFormatter.kt
@@ -1,35 +1,43 @@
 package com.configcat
 
+import platform.Foundation.NSNumber
+import platform.Foundation.NSNumberFormatter
+import platform.Foundation.NSNumberFormatterStyle
+import kotlin.math.abs
+
 internal actual fun doubleToString(doubleToString: Double): String {
-    return commonDoubleToString(doubleToString)
-    // return doubleToString.toString()
+    // TODO remove commonDoubleToString if not used any more
+    // return commonDoubleToString(doubleToString)
+
     // Handle Double.NaN, Double.POSITIVE_INFINITY and Double.NEGATIVE_INFINITY
-//        if (doubleToString.isNaN() || doubleToString.isInfinite()) {
-//            return doubleToString.toString()
-//        }
+    if (doubleToString.isNaN() || doubleToString.isInfinite()) {
+        return doubleToString.toString()
+    }
 
     // To get similar result between different SDKs the Double value format is modified.
     // Between 1e-6 and 1e21 we don't use scientific-notation. Over these limits scientific-notation used but the
     // ExponentSeparator replaced with "e" and "e+".
     // "." used as decimal separator in all cases.
-//        val abs = abs(doubleToString)
-//        val formatter = NSNumberFormatter()
-//        formatter.minimumFractionDigits = 0u
-//        formatter.maximumFractionDigits = 2u
-//        formatter.numberStyle = 1u //Decimal
-//        return formatter.stringFromNumber(NSNumber(double))!!
-//        val fmt =
-//            if (1e-6 <= abs && abs < 1e21) DecimalFormat("#.#################") else DecimalFormat("#.#################E0")
-//        val symbols = DecimalFormatSymbols.getInstance(Locale.UK)
-//        if (abs > 1) {
-//            symbols.exponentSeparator = "e+"
-//        } else {
-//            symbols.exponentSeparator = "e"
-//        }
-//        fmt.decimalFormatSymbols = symbols
-//        return fmt.format(doubleToString)
+    val abs = abs(doubleToString)
+    val formatter = NSNumberFormatter()
+    formatter.minimumFractionDigits = 0u
+    formatter.maximumFractionDigits = 17u
+    if (1e-6 <= abs && abs < 1e21) {
+        formatter.numberStyle = NSNumberFormatterStyle.NSNumberFormatterDecimalStyle
+    } else {
+        formatter.exponentSymbol = "e"
+        formatter.numberStyle = NSNumberFormatterStyle.NSNumberFormatterScientificStyle
+    }
+    return formatter.stringFromNumber(NSNumber(doubleToString))
+
 }
 
 internal actual fun formatDoubleForLog(doubleToFormat: Double): String {
-    return commonFormatDoubleForLog(doubleToFormat)
+    // TODO remove commonFormatDoubleForLog if not used any more
+    // return commonFormatDoubleForLog(doubleToFormat)
+    val formatter = NSNumberFormatter()
+    formatter.minimumFractionDigits = 0u
+    formatter.maximumFractionDigits = 4u
+    formatter.numberStyle = NSNumberFormatterStyle.NSNumberFormatterDecimalStyle
+    return formatter.stringFromNumber(NSNumber(doubleToFormat))
 }
