e305ca786a74258d29cb3a98ac1be82190ae12ee
novalisdenahi <novalisdenahi@gmail.com>
2024-10-31 16:57:54 +0100

    update sample projects

--- samples/kotlin/build.gradle.kts
+++ samples/kotlin/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.7.10"
+    kotlin("jvm") version "2.0.20"
     application
 }
 
@@ -10,13 +10,14 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    mavenLocal()
     maven {
         url = uri("https://oss.sonatype.org/content/repositories/snapshots")
     }
 }
 
 dependencies {
-    implementation("com.configcat:configcat-kotlin-client:3.0.0")
+    implementation("com.configcat:configcat-kotlin-client:3.2.0")
 }
 
 tasks.withType<KotlinCompile> {
